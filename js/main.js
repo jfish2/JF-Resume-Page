eeadfbcd540a9f7a01b06218e191bc1cc8ee3fec
Jonathan Fisher <jonathan@trueanthem.com>
2022-12-22 22:32:08 -0800

    added dark light mode toggling feature

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const root = document.documentElement // or document.querySelector(':root');
+//initially set to light mode
+let mode = 'light';
+let backgroundLight = getComputedStyle(root).getPropertyValue("--mainBgColor-light");
+let mainTextLight = getComputedStyle(root).getPropertyValue("--mainTextColor-light");
+let secondaryTextLight = getComputedStyle(root).getPropertyValue("--secondaryTextColor-light");
+let mainLinkLight = getComputedStyle(root).getPropertyValue("--mainLinkColor-light");
+let borderLight = getComputedStyle(root).getPropertyValue("--mainBorderColor-light");
+
+let backgroundDark = getComputedStyle(root).getPropertyValue("--mainBgColor-dark");
+let mainTextDark = getComputedStyle(root).getPropertyValue("--mainTextColor-dark");
+let secondaryTextDark = getComputedStyle(root).getPropertyValue("--secondaryTextColor-dark");
+let mainLinkDark = getComputedStyle(root).getPropertyValue("--mainLinkColor-dark");
+let borderDark = getComputedStyle(root).getPropertyValue("--mainBorderColor-dark");
+
+const colorModeButton = document.querySelector('#toggle');
+colorModeButton.addEventListener('click',toggleMode);
+
+function toggleMode() {
+
+  console.log('toggling mode');
+  if(mode === 'light') {
+    document.documentElement.style
+      .setProperty('--mainBgColor', backgroundDark);
+    document.documentElement.style
+      .setProperty('--mainBorderColor', borderDark);
+    document.documentElement.style
+      .setProperty('--mainLinkColor', mainLinkDark);
+    document.documentElement.style
+      .setProperty('--mainTextColor', mainTextDark);
+    document.documentElement.style
+      .setProperty('--secondaryTextColor', secondaryTextDark);
+
+    mode = 'dark';
+  } else {
+    document.documentElement.style
+      .setProperty('--mainBgColor', backgroundLight);
+    document.documentElement.style
+      .setProperty('--mainBorderColor', borderLight);
+    document.documentElement.style
+      .setProperty('--mainLinkColor', mainLinkLight);
+    document.documentElement.style
+      .setProperty('--mainTextColor', mainTextLight);
+    document.documentElement.style
+      .setProperty('--secondaryTextColor', secondaryTextLight);
+    mode = 'light'
+  }
+
+
+
+
+}
