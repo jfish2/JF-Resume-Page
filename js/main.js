f51e4f5172ec99dcb70788c747f6c4d51e68ab39
Jonathan Fisher <jonathan@trueanthem.com>
2023-03-02 23:34:12 -0800

    refactor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const root = document.documentElement // or document.querySelector(':root');
-//initially set to light mode
-//let mode = 'light';
-const storedMode = setInitialModeFromLocalStorage(); // gets mode from browser's local storage to ensure that page reload doesn't reset mode or light mode if no value
+//initially grab from local storage or set to light mode
+const storedMode = getInitialMode(); // gets mode from browser's local storage to ensure that page reload doesn't reset mode or light mode if no value
 
 let backgroundLight = getComputedStyle(root).getPropertyValue("--mainBgColor-light");
 let mainTextLight = getComputedStyle(root).getPropertyValue("--mainTextColor-light");
@@ -15,90 +14,61 @@ let secondaryTextDark = getComputedStyle(root).getPropertyValue("--secondaryText
 let mainLinkDark = getComputedStyle(root).getPropertyValue("--mainLinkColor-dark");
 let borderDark = getComputedStyle(root).getPropertyValue("--mainBorderColor-dark");
 
-if (storedMode === 'dark') {
-  document.documentElement.style
-    .setProperty('--mainBgColor', backgroundDark);
-  document.documentElement.style
-    .setProperty('--mainBorderColor', borderDark);
-  document.documentElement.style
-    .setProperty('--mainLinkColor', mainLinkDark);
-  document.documentElement.style
-    .setProperty('--mainTextColor', mainTextDark);
-  document.documentElement.style
-    .setProperty('--secondaryTextColor', secondaryTextDark);
-
-} else {
-  document.documentElement.style
-    .setProperty('--mainBgColor', backgroundLight);
-  document.documentElement.style
-    .setProperty('--mainBorderColor', borderLight);
-  document.documentElement.style
-    .setProperty('--mainLinkColor', mainLinkLight);
-  document.documentElement.style
-    .setProperty('--mainTextColor', mainTextLight);
-  document.documentElement.style
-    .setProperty('--secondaryTextColor', secondaryTextLight);
-
-}
-
 const colorModeButton = document.querySelector('#toggle');
 colorModeButton.addEventListener('click',toggleMode);
 
+
+if (storedMode === 'dark') {
+  setDarkMode();
+} else {
+  setLightMode();
+}
+
+function setDarkMode() {
+  root.style
+    .setProperty('--mainBgColor', backgroundDark);
+  root.style
+    .setProperty('--mainBorderColor', borderDark);
+  root.style
+    .setProperty('--mainLinkColor', mainLinkDark);
+  root.style
+    .setProperty('--mainTextColor', mainTextDark);
+  root.style
+    .setProperty('--secondaryTextColor', secondaryTextDark);
+}
+
+function setLightMode() {
+  root.style
+    .setProperty('--mainBgColor', backgroundLight);
+  root.style
+    .setProperty('--mainBorderColor', borderLight);
+  root.style
+    .setProperty('--mainLinkColor', mainLinkLight);
+  root.style
+    .setProperty('--mainTextColor', mainTextLight);
+  root.style
+    .setProperty('--secondaryTextColor', secondaryTextLight);
+}
+
 function toggleMode() {
-  const storedMode = localStorage.getItem('storedTheme')
+  const storedMode = localStorage.getItem('storedMode')
   console.log(storedMode);
   console.log('toggling mode');
   if (storedMode === 'light') {
-    document.documentElement.style
-      .setProperty('--mainBgColor', backgroundDark);
-    document.documentElement.style
-      .setProperty('--mainBorderColor', borderDark);
-    document.documentElement.style
-      .setProperty('--mainLinkColor', mainLinkDark);
-    document.documentElement.style
-      .setProperty('--mainTextColor', mainTextDark);
-    document.documentElement.style
-      .setProperty('--secondaryTextColor', secondaryTextDark);
-
-    localStorage.setItem('storedTheme', 'dark');
-   // mode = 'dark';
+    setDarkMode();
+    localStorage.setItem('storedMode', 'dark');
   } else {
-    document.documentElement.style
-      .setProperty('--mainBgColor', backgroundLight);
-    document.documentElement.style
-      .setProperty('--mainBorderColor', borderLight);
-    document.documentElement.style
-      .setProperty('--mainLinkColor', mainLinkLight);
-    document.documentElement.style
-      .setProperty('--mainTextColor', mainTextLight);
-    document.documentElement.style
-      .setProperty('--secondaryTextColor', secondaryTextLight);
-
-    localStorage.setItem('storedTheme', 'light');
-   // mode = 'light'
+   setLightMode();
+   localStorage.setItem('storedMode', 'light');
   }
-  //setCurrentModeInLocalStorage(mode);
 }
 
-  function setCurrentModeInLocalStorage(mode) {
-    let storedTheme = localStorage.getItem(mode);
-    if (!storedTheme) {
-      storedTheme = 'light';
-      localStorage.setItem('storedTheme', 'light');
+function getInitialMode() {
+    let storedMode = localStorage.getItem('storedMode');
+    if (!storedMode) {
+      storedMode = 'light';
+      localStorage.setItem('storedMode', 'light');
     }
-    else if (storedTheme === 'light') {
-      localStorage.setItem('storedTheme', 'dark');
-    } else if (storedTheme === 'dark') {
-      localStorage.setItem('storedTheme', 'light');
-    }
-  }
-
-  function setInitialModeFromLocalStorage() {
-    let storedTheme = localStorage.getItem('storedTheme');
-    if (!storedTheme) {
-      storedTheme = 'light';
-      localStorage.setItem('storedTheme', 'light');
-    }
-    return storedTheme;
+    return storedMode;
   }
 
